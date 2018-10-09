f946a678ec063a429943fdf56ad5ffa9a9b9b417
Adam Goodwin <adamgoodwin12345@gmail.com>
2018-10-08 18:25:11 -0700

    initial commit

---
+++ HeapSort/Heap.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HeapSort
+{
+    class Heap<T> where T : IComparable
+    {
+        private T[] heap;
+        private readonly bool max;
+        public int Capacity { get; private set; }
+        public int Count { get; private set; }
+
+        public Heap(bool max = true, int capacity = 10)
+        {
+            this.max = max;
+            Capacity = capacity;
+            heap = new T[Capacity];
+            Count = 0;
+        }
+
+        public void Add(T element)
+        {
+            if (Count == Capacity)
+            {
+                Capacity *= 2;
+
+                T[] temp = new T[Capacity];
+
+                for (int i = 0; i < heap.Length; i++)
+                {
+                    temp[i] = heap[i];
+                }
+                temp[heap.Length] = element;
+                heap = temp;
+            }
+            else
+            {
+                heap[Count] = element;
+            }
+            Count++;
+            HeapifyUp(Count - 1);
+        }
+
+        public T Pop()
+        {
+            T temp = heap[0];
+            heap[0] = heap[Count - 1];
+            heap[Count - 1] = default(T);
+            Count--;
+            HeapifyDown(0);
+            return temp;
+        }
+
+        public void HeapifyUp(int index)
+        {
+            if (index == 0)
+            {
+                return;
+            }
+
+            int parentIndex = (index - 1) / 2;
+
+            bool parentIsLess = heap[parentIndex].CompareTo(heap[index]) < 0;
+
+            if ((parentIsLess && max) || (!parentIsLess && !max))
+            {
+                T temp = heap[index];
+                heap[index] = heap[parentIndex];
+                heap[parentIndex] = temp;
+
+                HeapifyUp(parentIndex);
+            }
+
+        }
+
+        public void HeapifyDown(int index)
+        {
+            int leftChild = (index * 2) + 1;
+            int rightChild = leftChild + 1;
+
+            if (leftChild >= Count)
+            {
+                return;
+            }
+
+            if (rightChild >= Count)
+            {
+                bool leftIsBigger = heap[leftChild].CompareTo(heap[index]) > 0;
+                if ((leftIsBigger && max) || (!leftIsBigger && !max))
+                {
+                    T temp = heap[index];
+                    heap[index] = heap[leftChild];
+                    heap[leftChild] = temp;
+                }
+                return;
+            }
+
+            bool biggerThanBothChildren = heap[index].CompareTo(heap[leftChild]) > 0 && heap[index].CompareTo(heap[rightChild]) > 0;
+            if ((biggerThanBothChildren && max) || (!biggerThanBothChildren && !max))
+            {
+                return;
+            }
+
+            bool leftBiggerThanRight = heap[leftChild].CompareTo(heap[rightChild]) > 0;
+            if ((leftBiggerThanRight && max) || (!leftBiggerThanRight && !max))
+            {
+                T temp = heap[index];
+                heap[index] = heap[leftChild];
+                heap[leftChild] = temp;
+                HeapifyDown(leftChild);
+            }
+            else
+            {
+                T temp = heap[index];
+                heap[index] = heap[rightChild];
+                heap[rightChild] = temp;
+                HeapifyDown(rightChild);
+            }
+        }
+
+        public void Print()
+        {
+            int row = 1;
+            int col = 1;
+            for (int i = 0; i < Count; i++)
+            {
+                for (int j = 0; j < 5 - row; j++)
+                {
+                    Console.Write("    ");
+                }
+                Console.Write(heap[i]);
+                if (col >= (int)Math.Pow(2, row - 1))
+                {
+                    Console.WriteLine();
+                    col = 1;
+                    row++;
+                }
+                else
+                {
+                    col++;
+                }
+            }
+        }
+    }
+}
---
+++ HeapSort/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace HeapSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Heap<int> heap = new Heap<int>(false);
+
+            Random r = new Random();
+
+            for (int i = 0; i < 15; i++)
+            {
+                int num = r.Next(100) + 1;
+                Console.Write(num + " ");
+                heap.Add(num);
+            }
+            Console.WriteLine("\n");
+            heap.Print();
+            Console.WriteLine("\n");
+
+            for (int i = 0; i < 15; i++)
+            {
+                Console.Write(heap.Pop() + " ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
